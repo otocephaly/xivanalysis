f605900a082c43c6af91f714a060e13a7febd968
Ryan Beck <37195907+Rjb99@users.noreply.github.com>
2024-10-21 21:57:21 -0400

    Disable 7.05 modules for pre 7.05 parses (#2132)
    * Disable 7.05 modules for pre 7.05 parses
    
    * Update changelog.tsx

--- src/parser/jobs/vpr/changelog.tsx
+++ src/parser/jobs/vpr/changelog.tsx
@@ -4,6 +4,11 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2024-10-20'),
+		Changes: () => <>Disabled feedback modules that were implmented for 7.05 Viper Changes</>,
+		contributors: [CONTRIBUTORS.RYAN],
+	},
+	{
 		date: new Date('2024-10-02'),
 		Changes: () => <>Expand <DataLink action="SERPENTS_TAIL"/> checklist and add <DataLink action="VICEWINDER"/> and <DataLink action="VICEPIT"/> checklist.</>,
 		contributors: [CONTRIBUTORS.RYAN],
